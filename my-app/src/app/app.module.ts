d47a83145fd9a804276c8c6fee1c38dbb8d7206f
Edwin <chespirana@gmail.com>
2020-02-12 21:49:45 -0600

    Creating same components as tutorial

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {TopBarComponent} from './top-bar/top-bar.component';
+import {RouterModule} from '@angular/router';
+import {ProductListComponent} from './product-list/product-list.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+    {path: '', component: ProductListComponent}]
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
